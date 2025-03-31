022a4bcc296934fc76d7fa1c226c397dc835e078
KSY726 0 <syku0903@gmail.com>
2025-03-31 03:22:02 +0000

    0331

---
+++ aski.py
@@ -0,0 +1,14 @@
+#아스키 코드 그림 출력하기
+
+def print_ascii_image():
+    ascii_art = """
+      *****  
+     *     * 
+    *       *
+    *       *
+     *     * 
+      *****  
+    """
+    print(ascii_art)
+
+print_ascii_image()
---
+++ test2.py
@@ -0,0 +1,15 @@
+v = input("입력값: ")
+if v == 0:
+    print("0이다")
+if v != 0:
+    print("0d가 아니다")
+
+v = input
+print(type(v))
+print("숫자로 변환")
+v = int(v)
+print(type(v))
+if v == 0:
+    print("0이다")
+if v != 0:
+    print("0d이 아니다")
